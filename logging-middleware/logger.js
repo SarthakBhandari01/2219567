153eb08f60514e5c6927510960d7c816a07ba60b
Sarthak <sarthakbhandari49@gmail.com>
2025-07-15 14:24:40 +0530

    Added token

--- logging-middleware/logger.js
+++ logging-middleware/logger.js
@@ -1,18 +1,19 @@
+// logging-middleware/logger.js
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 const LOGGING_URL = "http://20.244.56.144/evaluation-service/logs";
+const LOG_TOKEN = process.env.TOKEN; 
 
 const log = async (stack, level, pkg, message) => {
   try {
-    await axios.post(LOGGING_URL, {
-      stack,
-      level,
-      package: pkg,
-      message,
-    });
-  } catch (_) {
-    // ignore
-  }
+    await axios.post(
+      LOGGING_URL,
+      { stack, level, package: pkg, message },
+      { headers: { Authorization: `Bearer ${LOG_TOKEN}` } }
+    );
+  } catch (_) {}
 };
 
 export default log;
